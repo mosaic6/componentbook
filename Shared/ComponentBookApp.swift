4a3d8b95dcb9010db1b19185f1b30148b743bacc
Joshua Walsh <jwalsh@bignerdranch.com>
2021-09-02 12:55:11 -0400

    Initial Commit

---
+++ Shared/ComponentBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentBookApp.swift
+//  Shared
+//
+//  Created by Joshua Walsh on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ComponentBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
